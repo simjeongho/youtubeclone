f4c0fe2e454b89a8da6793171c297b4872c9de73
simjeongho <simjeongho012@gmail.com>
2021-07-11 21:08:47 +0900

    video list애니매이션 추가

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import styles from './video_item.module.css';
 
 const Videoitem = (props) => (
-    <>
-            <h1>{props.video.snippet.title}</h1>
-            <img src={props.video.snippet.thumbnails.url} alt={props.video.snippet.channelTitle} />
-    </>
+    <li className= {styles.video}>
+            
+            <img className= {styles.thumbnails} src={props.video.snippet.thumbnails.medium.url} alt={props.video.snippet.channelTitle} />
+            <div >
+                <p className= {styles.title}>{props.video.snippet.title}</p>
+                <p className= {styles.channel}>{props.video.snippet.channelTitle}</p>
+            </div>
+    </li>
     );
 
 export default Videoitem;
