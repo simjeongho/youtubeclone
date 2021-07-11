6bd575a2db75e18b2a32fb5d9be03e02a60d6a5a
simjeongho <simjeongho012@gmail.com>
2021-07-12 00:26:13 +0900

    css수정

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import styles from './video_item.module.css';
 
 const Videoitem = (props) => (
-    <li className= {styles.video}>
-            
+    <li className= {styles.container}>
+            <div className={styles.video}>
             <img className= {styles.thumbnails} src={props.video.snippet.thumbnails.medium.url} alt={props.video.snippet.channelTitle} />
-            <div >
+            <div className= {styles.metadata} >
                 <p className= {styles.title}>{props.video.snippet.title}</p>
                 <p className= {styles.channel}>{props.video.snippet.channelTitle}</p>
+            </div>
             </div>
     </li>
     );
