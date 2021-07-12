932cd10b8c62aa75637233564f40f6bd9ceee946
simjeongho <simjeongho012@gmail.com>
2021-07-12 17:37:38 +0900

    비동기 함수로 변환

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -9,23 +9,24 @@ class Youtube
           };
     }
 
-    mostPopular()
+    async mostPopular()
     {
-        return fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
-         this.getRequestOptions)
-        .then(response => response.json())
-        .then(result => result.items)
-        .catch(error => console.log('error', error));
+        try {
+            const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
+                this.getRequestOptions);
+            const result_1 = await response.json();
+            return result_1.items;
+        } catch (error) {
+            return console.log('error', error);
+        }
     }
 
-    search(query)
+    async search(query)
     {
-       return fetch
-       (`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
-        this.getRequestOptions)
-      .then(response => response.json())
-      .then(result => 
-        result.items.map(item =>({...item, id:item.id.videoId})));
+       const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
+            this.getRequestOptions);
+        const result_1 = await response.json();
+        return result_1.items.map(item => ({ ...item, id: item.id.videoId }));
       
     }
     
